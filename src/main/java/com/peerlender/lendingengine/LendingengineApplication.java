4a64d98ef5fd67b6f2d6dd70850c2191ae8acf12
Mindaugas <mindaugas.barcauskas@mif.stud.vu.lt>
2020-01-08 20:42:05 +0200

    first commit

---
+++ src/main/java/com/peerlender/lendingengine/LendingengineApplication.java
@@ -0,0 +1,14 @@
+package com.peerlender.lendingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LendingengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingengineApplication.class, args);
+	}
+
+}
