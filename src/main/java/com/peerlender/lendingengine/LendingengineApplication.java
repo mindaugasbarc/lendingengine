c6202427bde7c569d9e03d27f4513ff4fbdd0d0e
Mindaugas <mindaugas.barcauskas@mif.stud.vu.lt>
2020-01-11 16:23:30 +0200

    adding user seed data + find all users endpoint

--- src/main/java/com/peerlender/lendingengine/LendingengineApplication.java
+++ src/main/java/com/peerlender/lendingengine/LendingengineApplication.java
@@ -1,14 +1,27 @@
 package com.peerlender.lendingengine;
 
+import com.peerlender.lendingengine.domain.model.User;
+import com.peerlender.lendingengine.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class LendingengineApplication {
+public class LendingengineApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LendingengineApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User(1, "John", "B", 27, "Software Developer"));
+		userRepository.save(new User(2, "Peter", "C", 21, "Pilot"));
+		userRepository.save(new User(3, "Henry", "E", 21, "Unemployed"));
+	}
 }
