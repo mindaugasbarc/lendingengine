3038eb443f66b42bd9d64847a6166d9f15f26879
Mindaugas <mindaugas.barcauskas@mif.stud.vu.lt>
2020-01-11 17:25:21 +0200

    transforming loan request to loan adapter develop

--- src/main/java/com/peerlender/lendingengine/application/LoanController.java
+++ src/main/java/com/peerlender/lendingengine/application/LoanController.java
@@ -1,9 +1,11 @@
 package com.peerlender.lendingengine.application;
 
 import com.peerlender.lendingengine.application.model.LoanRequest;
+import com.peerlender.lendingengine.domain.model.LoanApplication;
 import com.peerlender.lendingengine.domain.model.User;
-import com.peerlender.lendingengine.domain.repository.LoanRequestRepository;
+import com.peerlender.lendingengine.domain.repository.LoanApplicationRepository;
 import com.peerlender.lendingengine.domain.repository.UserRepository;
+import com.peerlender.lendingengine.domain.service.LoanApplicationAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,18 +17,22 @@ import java.util.List;
 @RestController
 public class LoanController {
 
-    private final LoanRequestRepository loanRequestRepository;
+    private final LoanApplicationRepository loanApplicationRepository;
     private final UserRepository userRepository;
+    private final LoanApplicationAdapter loanApplicationAdapter;
 
     @Autowired
-    public LoanController(LoanRequestRepository loanRequestRepository, UserRepository userRepository) {
-        this.loanRequestRepository = loanRequestRepository;
+    public LoanController(LoanApplicationRepository loanApplicationRepository,
+                          UserRepository userRepository,
+                          LoanApplicationAdapter loanApplicationAdapter) {
+        this.loanApplicationRepository = loanApplicationRepository;
         this.userRepository = userRepository;
+        this.loanApplicationAdapter = loanApplicationAdapter;
     }
 
     @PostMapping(value = "/loan/request")
-    public void requestLoan(@RequestBody final LoanRequest loanApplication) {
-
+    public void requestLoan(@RequestBody final LoanRequest loanRequest) {
+        loanApplicationRepository.save(loanApplicationAdapter.transform(loanRequest));
     }
 
     @GetMapping(value = "/users")
